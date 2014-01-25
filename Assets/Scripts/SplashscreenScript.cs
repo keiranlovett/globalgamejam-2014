680846a5d4f546ec36187bc1743636dafaea1cea
Keiran <kvclovett@gmail.com>
2014-01-26 00:32:15 +0800

    Added Alex's changes including network and gui, implementation of art assets and menus

---
+++ Assets/Scripts/SplashscreenScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashscreenScript : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(4);
+		Application.LoadLevel(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TitleGUIHandler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleGUIHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		WaitingRoomScript.RefreshHostList();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (WaitingRoomScript.isRefreshingHostList && MasterServer.PollHostList().Length > 0)
+		{
+			WaitingRoomScript.isRefreshingHostList = false;
+			WaitingRoomScript.hostList = MasterServer.PollHostList();
+		}
+	}
+	
+	void OnGUI () {
+
+		if (GUI.Button (new Rect((int)(Screen.width / 2) - 150, 200, 300, 25), "Create Game")) {
+			WaitingRoomScript.StartServer(System.Environment.UserName);
+			Application.LoadLevel(2);
+		}
+
+		int i = 0;
+
+		if (WaitingRoomScript.hostList != null) {
+			for (i = 0; i < WaitingRoomScript.hostList.Length; i++)
+			{
+				if (GUI.Button(new Rect((int)(Screen.width / 2) - 150, 175 + (50 * (i+1)), 300, 50), WaitingRoomScript.hostList[i].gameName)) {
+					WaitingRoomScript.JoinServer(WaitingRoomScript.hostList[i]);
+				}
+			}
+		}
+
+		GUI.Box(new Rect((int)(Screen.width / 2) - 160, 165 + 10, 320, 10 + (50 * (i+1))), "Games");
+
+		if (GUI.Button (new Rect ((int)(Screen.width / 2) - 100, 175 + (50 * (i+1)) + 10, 200, 25), "Refresh")) {
+			WaitingRoomScript.RefreshHostList();
+		}
+
+		if (GUI.Button (new Rect ((int)Screen.width - 80, 20, 60, 40), "Exit")) {
+			Application.Quit();
+		}
+
+	}
+	
+	
+}
